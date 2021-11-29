88894aaa1d0f8f6062e922c7d9835a23541edff9
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-29 13:36:20 +0100

    presis configered

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import configureStore from "./redux";
+import configureStore, { persister } from "./redux";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
-      <App />
+      <PersistGate persistor={persister}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,9 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import searchResultsReducer from "./reducers/search";
 import favoritesListReducer from "./reducers/favorites";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
 const workingMiddleware =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -17,15 +20,29 @@ export const initialState = {
   },
 };
 
+const persistConfig = {
+  key: "root", // this is referring to the entire stor object
+  storage, // this is referring to the local storage
+  transforms: [
+    // this is encrypting the object that gets stored in the local storage so the user cannot manipulate
+    encryptTransform({
+      secretKey: process.env.REACT_APP_STORE_ENCRYPT_KEY, //this key is a key that is stored in the dotEnv file that is in your local machine
+    }), // This key can be anything you want it to be
+  ],
+};
+
 const mainReducer = combineReducers({
   searchResults: searchResultsReducer,
   favoritesList: favoritesListReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
 const configureStore = createStore(
-  mainReducer,
+  persistedReducer,
   initialState,
   workingMiddleware(applyMiddleware(thunk))
 );
 
 export default configureStore;
+export const persister = persistStore(configureStore);
